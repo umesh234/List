d611c711e05edf72d36c725ce2fc06d89b10c570
umesh <ukumar@gmail.com>
2013-09-28 22:16:49 -0700

    added a bit more functions

--- src/org/umesh/utils/List/LinkList.java
+++ src/org/umesh/utils/List/LinkList.java
@@ -1,5 +1,6 @@
 package org.umesh.utils.List;
 
+
 public class LinkList {
 
 	public static class ListNode {
@@ -24,6 +25,27 @@ public class LinkList {
 		head = null ;
 	}
 	
+	public LinkList( int ... nums) {
+		this();
+		for( int i=0;i< nums.length;i++) {
+			append(nums[i]);
+		}
+	}
+	
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append(" {");
+		ListNode cur = head ;
+		while( cur != null) {
+			str.append (cur.data);
+			str.append (",");
+			cur = cur.next;
+		}
+		str.deleteCharAt(str.length()-1);
+		str.append("}");
+		return str.toString();
+	}
+	
 	public boolean append(int data) {
 		if ( head == null) {
 			head = new ListNode(data , null);
@@ -34,6 +56,59 @@ public class LinkList {
 			cur = cur.next ;
 		cur.next = new ListNode(data , null);
 		return true;
+	}
+
+	public boolean prepend( int data ) {
+		head = new ListNode( data , head);
+		return true;
+	}
+	
+	//returns true if the list has a cycle inside it
+	public boolean isCircular( ) {
+		ListNode slow = head ;
+		ListNode fast = head ; 
+		
+		while ( slow != null && fast != null && slow!= fast) {
+			slow = slow.next;
+			
+			//increment fast twice
+			fast = fast.next ;
+			if( fast != null)
+				fast = fast.next;
+		
+		}// end of while loop
+		
+		if ( fast == null || slow == null) 
+			return false;
+		return true;
+		
+	}
+	
+	//returns m th last node from end ( m =0 is last of list )
+	public ListNode fromLast( int index) {
+		
+		//check input for correctness 
+		//really throw exception here
+		if( index < 0 || head == null) return null;
+		
+		// skip first index+1 elements
+		ListNode back = head;
+		ListNode frontRunner = head;
+		while( frontRunner != null && index > -1 ) {
+			index--;
+			frontRunner = frontRunner.next;
+		}
+		
+		//if we ran out of elements than return -1 
+		if( index > -1) 
+			return null ;
+		
+		while ( frontRunner != null) {
+			frontRunner = frontRunner.next;
+			back = back.next;
+		}
+		return back;
+		
 	}
 	
 	public int size() {
--- tst/org/umesh/utils/List/LinkListTest.java
+++ tst/org/umesh/utils/List/LinkListTest.java
@@ -25,8 +25,7 @@ public class LinkListTest {
 	@Parameters
 	public static Collection<Object[]> generateData() {
 		 Object[][] testCases = {
-				 				  { 2,1,1 },
-				 				 { 2,4,3 }
+				 				  { 2,1,1 }
 		 						};
 		return Arrays.asList(testCases);
 	 }
@@ -40,6 +39,20 @@ public class LinkListTest {
 		
 		int result = ll.size();
 		assertEquals( " TestArgs: " + numbers , expectedSize , result);
+	}
+	
+	@Test 
+	public void testLists() {
+		LinkList l = new LinkList(3,4,5,6);
+		System.out.println(l);
+		testFromLast(l);
+	}
+	
+	public void testFromLast(LinkList l) {
+		assertEquals ( "List Length : 4 " , 4 , l.size() );
+		for ( int i=-2;i< l.size() +2;i++){
+			System.out.println(" list is " + l + " i is " + i + " from last is " + l.fromLast(i) );
+		}
 	}
 	
 
